fed58806d6dce5ee8f4c3ac0d219ae9f504be989
Salman-18 <salmannizamani205@gmail.com>
2023-08-30 15:21:07 +0500

    Chess club

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+
+# Set page title and icon
+st.set_page_config(page_title="Nizamani Chess Club", page_icon="♟️")
+
+# Navigation
+st.sidebar.title("Navigation")
+menu = st.sidebar.radio("Go to", ["Home", "Rating", "About"])
+
+# Home Section
+if menu == "Home":
+    st.title("Welcome to Nizamani Chess Club")
+    player_data = [
+        {"name": "ishaque nizamani", "rating": 1388},
+        {"name": "ammar nizamani", "rating": 1883},
+        {"name": "hamid nizamani", "rating" : 1632},
+        {"name": "salman nizamani", "rating": 832}
+        # Add more player data streamlit run d:/ChessClubWeb/app.py
+    ]
+    for player in player_data:
+        st.write(f"**{player['name']}**")
+        image_path = f"assets/{player['name']}.jpg"
+        st.image(image_path, width=128, caption=player['name'])
+        st.write(f"Rating: {player['rating']}")
+# Rating Section
+elif menu == "Rating":
+    st.title("Player Ratings")
+    players_data = [
+        {"name": "ishaque nizamani", "rating": 1388},
+        {"name": "ammar nizamani", "rating": 1883},
+        {"name": "hamid nizamani", "rating" : 1632},
+        {"name": "salman nizamani", "rating": 832}
+#Add more player data
+    ]
+
+    rating_data = {player['name']: player['rating'] for player in players_data}
+        
+    st.table({"Name": list(rating_data.keys()), "Rating": list(rating_data.values())})
+
+# About Section
+else:
+    st.title("About Us")
+    st.write("We are Nizamani Chess Club, dedicated to promoting the love of chess in Pakistan and beyond.")
+
+# Footer
+st.sidebar.write("All rights belong to Salman Nizamani")
+
+
